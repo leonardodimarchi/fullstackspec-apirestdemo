d0516d91b551b1a69b886805a495ff021c850099
Leonardo Dimarchi <62081192+leonardodimarchi@users.noreply.github.com>
2026-05-30 13:31:06 -0300

    feat: add a new temperature conversion endpoint

--- src/main/java/com/leo/fullstackspec_apirestdemo/controllers/TemperaturaController.java
+++ src/main/java/com/leo/fullstackspec_apirestdemo/controllers/TemperaturaController.java
@@ -3,10 +3,7 @@ package com.leo.fullstackspec_apirestdemo.controllers;
 import com.leo.fullstackspec_apirestdemo.dtos.TemperaturaResponse;
 import com.leo.fullstackspec_apirestdemo.services.TemperaturaService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/temperatura")
@@ -21,6 +18,26 @@ public class TemperaturaController {
             @RequestParam(required = false) String de,
             @RequestParam(required = false) String para,
             @RequestParam(required = false) Double valor
+    ) {
+        return service.convert(de, para, valor);
+    }
+
+    // Parâmetros required=false para realizar validações no service (ideal seria usar o Jakarta Validation)
+    @GetMapping("/convert/{valor}/de/{de}/para/{para}")
+    public TemperaturaResponse convertFromPath(
+            @PathVariable(required = false) String de,
+            @PathVariable(required = false) String para,
+            @PathVariable(required = false) Double valor
+    ) {
+        return service.convert(de, para, valor);
+    }
+
+    // Parâmetros required=false para realizar validações no service (ideal seria usar o Jakarta Validation)
+    @GetMapping("/convert/{valor}/{de}/{para}")
+    public TemperaturaResponse convertFromPathFromExercise(
+            @PathVariable(required = false) String de,
+            @PathVariable(required = false) String para,
+            @PathVariable(required = false) Double valor
     ) {
         return service.convert(de, para, valor);
     }
